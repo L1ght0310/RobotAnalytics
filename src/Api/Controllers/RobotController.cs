a66dc3816548ac8938b417e819d66bfd9c3c98e0
L1ght <hikaruwatanabe0310@gmail.com>
2026-07-12 16:21:14 +0900

    Controller実装

---
+++ src/Api/Controllers/RobotController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RobotAnalytics.Application.UseCases;
+using RobotAnalytics.Contracts.Commands;
+
+namespace RobotAnalytics.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RobotController : ControllerBase
+{
+    private readonly MoveRobotUseCase _moveRobotUseCase;
+
+    public RobotController(MoveRobotUseCase moveRobotUseCase)
+    {
+        _moveRobotUseCase = moveRobotUseCase;
+    }
+
+    [HttpPost("movej")]
+    public Task MoveJAsync([FromBody] MoveJCommand moveJCommand)
+    {
+        return _moveRobotUseCase.ExecuteAsync(moveJCommand);
+    }
+}
